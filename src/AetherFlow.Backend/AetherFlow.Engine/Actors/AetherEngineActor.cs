73bdfe1a969d806063a376ab55998576d12da822
PatrickMisti <patrickm99@hotmail.de>
2026-06-07 23:22:44 +0200

    update EngineActor

--- src/AetherFlow.Backend/AetherFlow.Engine/Actors/AetherEngineActor.cs
+++ src/AetherFlow.Backend/AetherFlow.Engine/Actors/AetherEngineActor.cs
@@ -1,6 +1,9 @@
-﻿using AetherFlow.Shared.Messages.ShardRegion;
+﻿using AetherFlow.Domain.Domains;
+using AetherFlow.Shared.Messages.ShardRegion;
 using Akka.Event;
 using Akka.Persistence;
+using Akka.Streams;
+using Akka.Streams.Dsl;
 
 namespace AetherFlow.Engine.Actors;
 
@@ -8,18 +11,46 @@ public class AetherEngineActor : ReceivePersistentActor
 {
     public override string PersistenceId { get; } = Context.Self.Path.Name;
     private readonly ILoggingAdapter _log = Context.GetLogger();
+    private ISourceQueueWithComplete<AetherChunk>? _sourceQueue;
+    private readonly SharedKillSwitch _killSwitch;
 
-    public AetherEngineActor()
+    public AetherEngineActor(SharedKillSwitch? killSwitch = null)
     {
+        _killSwitch = killSwitch ?? KillSwitches.Shared("aether-engine-kill-switch");
         // Todo: handle chunk messages and perform anomaly detection
         // Todo: persist chunk messages and replay on recovery
         // Todo: make notifier for live view updates and send notifications on anomalies
         // setup should work now with akka.db 
-        Command<ChunkShardMessage>(HandleChunkMessage);
+        Command<ChunkShardMessage>(msg =>
+        {
+            _log.Debug("Got command {entityId} with send msg time: {created} time now: {now}",
+                msg.EntityId,
+                msg.Created,
+                DateTime.UtcNow);
+
+            _sourceQueue?.OfferAsync(msg.Chunk);
+            SaveSnapshot(msg);
+        });
+    }
+    
+    protected override void PreStart()
+    {
+        base.PreStart();
+        _sourceQueue = Source.Queue<AetherChunk>(bufferSize: 100, overflowStrategy: OverflowStrategy.DropHead)
+            .Via(_killSwitch.Flow<AetherChunk>())
+            .Via(Flow.Create<AetherChunk>().Select(msg =>
+            {
+                Console.WriteLine("processing.....");
+                return msg;
+            }))
+            .To(Sink.Ignore<AetherChunk>())
+            .Run(Context.System.Materializer());
     }
 
-    private void HandleChunkMessage(ChunkShardMessage msg)
+    protected override void PostStop()
     {
-        _log.Debug("Handling chunk message for entity {EntityId}", msg.EntityId);
+        _sourceQueue?.Complete();
+        _killSwitch.Shutdown();
+        base.PostStop();
     }
 }
