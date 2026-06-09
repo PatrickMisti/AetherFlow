05e6542712f527d0e06df8780b52e9935cfda23d
PatrickMisti <patrickm99@hotmail.de>
2026-06-09 21:28:02 +0200

    forgot to initialize CreatedUtc

--- src/AetherFlow.Backend/AetherFlow.Shared/Messages/Notifications/ChargingLevelNotification.cs
+++ src/AetherFlow.Backend/AetherFlow.Shared/Messages/Notifications/ChargingLevelNotification.cs
@@ -5,5 +5,5 @@ namespace AetherFlow.Shared.Messages.Notifications;
 public record ChargingLevelNotification(string EntityId, AetherChargeState Level): INotification
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public DateTime CreatedUtc { get; } = DateTime.UtcNow;
+    public DateTime CreatedUtc { get; init; } = DateTime.UtcNow;
 };
